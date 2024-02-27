ca6f2fe73d827cd4fbdae2ac4963805ce13244b5
Rayadi Ayoub <rayadiayoub7@gmail.com>
2024-02-27 10:06:55 +0100

    create  pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Pole from './pages/Pole';
+import Campany from './pages/Company';
+import Projects from './pages/Projects';
+import Home from './pages/Home';
 
 export default function App() {
+
   return (
-    <div className='text-3xl text-red-400'>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/> 
+        <Route path='/About' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='Register' element={<Register/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/pole' element={<Pole/>}/>
+        <Route path='/campany' element={<Campany/>}/> 
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Pole.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pole() {
+  return (
+    <div>
+      <h1>Pole</h1>
+    </div>
+  )
+}

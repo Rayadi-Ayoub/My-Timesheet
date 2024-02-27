64ba161ab1e616a372c78437a223a73777d040d6
Rayadi Ayoub <rayadiayoub7@gmail.com>
2024-02-27 14:46:12 +0100

    connect to db

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import Express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = Express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
